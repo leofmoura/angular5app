052597feae4fdc1bfc946555b392bc2e2ad19583
Leonardo Ferreira de Moura <leofmoura@gmail.com>
2018-02-27 00:56:16 -0500

    Adding component Banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
